30ef8d087bd3977822860c83e43ff3562512535f
Peter Carpenter <peterc1731@gmail.com>
2020-11-12 21:34:21 +0000

    refactor: remove not needed token

--- lib/prismic.ts
+++ lib/prismic.ts
@@ -1,11 +1,7 @@
 import Prismic from 'prismic-javascript';
 import { Article, Experiment, Project } from '../types/prismic';
 
-export const apiEndpoint = 'https://petercxyz.cdn.prismic.io/api/v2';
-export const accessToken =
-  'MC5YNnNqWXhNQUFDTUFBWHd0.77-977-9Te-_ve-_vSBEID_vv70677-9ORLvv71fVe-_vXLvv73vv71S77-977-977-977-9V2bvv73vv73vv70F';
-
-const Client = Prismic.client(apiEndpoint, { accessToken });
+const Client = Prismic.client('https://petercxyz.cdn.prismic.io/api/v2');
 
 export const getArticles = async (): Promise<Article[]> =>
   (await Client.query(Prismic.Predicates.at('document.type', 'article')))
